96125f2f9493db6da4828ba49fb0f865597734b4
Rafael Silvério <rafael.silverio11@gmail.com>
2020-04-13 11:41:25 -0300

    Implicitly detecting headers and adding basic validation for them.

--- src/main/scala/oystr/Utils.scala
+++ src/main/scala/oystr/Utils.scala
@@ -1,5 +1,21 @@
 package oystr
 
-class Utils {
+import org.apache.commons.lang3.StringUtils
+import play.api.Configuration
+import play.api.mvc.Request
 
+object Utils {
+    implicit class TokenHelper(request: Request[AnyRef]) {
+        def vaultToken()(implicit configuration: Configuration): Option[String] = extract("vault")
+        def morbidToken()(implicit configuration: Configuration): Option[String] = extract("morbid")
+
+        def validate(requireVaultToken: Boolean)(implicit configuration: Configuration): Boolean = {
+            morbidToken().isDefined && ((requireVaultToken && vaultToken().isDefined) || !requireVaultToken)
+        }
+
+        private def extract(key: String)(implicit configuration: Configuration): Option[String] = {
+            val header = configuration.get[String](s"$key.header")
+            request.headers.get(header).filter(StringUtils.isNotEmpty)
+        }
+    }
 }
--- src/main/scala/oystr/controllers/SecretsController.scala
+++ src/main/scala/oystr/controllers/SecretsController.scala
@@ -1,13 +1,14 @@
 package oystr.controllers
 
 import javax.inject.{Inject, Singleton}
+import oystr.Utils._
 import oystr.akka.Inquire.inquire
 import oystr.akka.RootActorsImpl
 import oystr.domain.json._
 import oystr.domain.{Content, Metadata, StoreDataRequest, TokenRequest}
 import oystr.services._
 import play.api.libs.json.{JsSuccess, JsValue}
-import play.api.mvc.{Action, AnyContent, InjectedController, Request, Result}
+import play.api.mvc._
 
 import scala.concurrent.Future
 
@@ -17,59 +18,59 @@ class SecretsController @Inject() (
     actors: RootActorsImpl) extends InjectedController {
     implicit val ec = services.ec()
     implicit val sys = services.sys()
+    implicit val configuration = services.conf()
 
-    private val vaultHeader = services.conf().get[String]("vault.header")
-    private val oystrHeader = services.conf().get[String]("oystr.header")
+    private val vaultHeader = configuration.get[String]("vault.header")
 
-    def createPolicyFor: Action[JsValue] = Action.async(parse.json) {
-        _.body.validate[Metadata] match {
-            case success: JsSuccess[Metadata] => ~> { CreatePolicyFor(success.get) }
+    def createPolicyFor: Action[JsValue] = Action.async(parse.json) { implicit request =>
+        request.body.validate[Metadata] match {
+            case success: JsSuccess[Metadata] => ~> (CreatePolicyFor(success.get), requireVaultToken = false)
         }
     }
 
-    def createTokenFor: Action[JsValue] = Action.async(parse.json) {
-        _.body.validate[TokenRequest] match {
-            case success: JsSuccess[TokenRequest] => ~> { CreateTokenFor(success.get) }
+    def createTokenFor: Action[JsValue] = Action.async(parse.json) { implicit request =>
+        request.body.validate[TokenRequest] match {
+            case success: JsSuccess[TokenRequest] => ~> (CreateTokenFor(success.get), requireVaultToken = false)
         }
     }
 
-    def storeCredentialsFor: Action[JsValue] = Action.async(parse.json) { request =>
+    def storeCredentialsFor: Action[JsValue] = Action.async(parse.json) { implicit request =>
         request.body.validate[StoreDataRequest] match {
             case success: JsSuccess[StoreDataRequest] => ~> {
-                if(request.headers.hasHeader(vaultHeader) || request.headers.hasHeader(oystrHeader)) {
-                    val token = request.headers(vaultHeader)
-                    val storeDataRequest = success.get
-                    val metadata = storeDataRequest.metadata.copy(token = Some(token))
-                    val content = storeDataRequest.content
+                val storeDataRequest = success.get
+                val metadata = storeDataRequest.metadata.copy(token = request.vaultToken())
+                val content = storeDataRequest.content
 
-                    if(content.password.isDefined) {
-                        StoreCredentialsFor(metadata, content)
-                    } else {
-                        StoreCertificateFor(metadata, content)
-                    }
+                if(content.password.isDefined) {
+                    StoreCredentialsFor(metadata, content)
                 } else {
-                    Future.successful(BadRequest(s"Missing $vaultHeader header."))
+                    StoreCertificateFor(metadata, content)
                 }
             }
         }
     }
 
-    def deleteDataFor(organization: String, name: String, bot: String, username: String, target: String): Action[AnyContent] = Action.async {
-        implicit r => <~(organization, name, bot, username, target, delete = true)
+    def deleteDataFor(organization: String, name: String, bot: String, username: String, target: String):
+        Action[AnyContent] = Action.async {
+        implicit r => <~ (organization, name, bot, username, target, delete = true)
     }
 
-    def dataFor(organization: String, name: String, bot: String, username: String, target: String): Action[AnyContent] = Action.async {
-        implicit r => <~(organization, name, bot, username, target)
+    def dataFor(organization: String, name: String, bot: String, username: String, target: String):
+        Action[AnyContent] = Action.async {
+        implicit r => <~ (organization, name, bot, username, target)
     }
 
-    private def ~>(msg: Any): Future[Result] = {
-        inquire(actors.vault) {
-            msg
-        } flatMap {
-            case future: Future[(Int, JsValue)] => future map toResponse
-        } recover {
-            case any => InternalServerError(s"Unknown internal server error. $any")
-        }
+    private def ~>(msg: Any, requireVaultToken: Boolean = true)(implicit request: Request[AnyRef]): Future[Result] = {
+        validateThen (
+            inquire(actors.vault) {
+                msg
+            } flatMap {
+                case future: Future[(Int, JsValue)] => future map toResponse
+            } recover {
+                case any => InternalServerError(s"Unknown internal server error. $any")
+            },
+            requireVaultToken
+        )
     }
 
     /**
@@ -77,11 +78,10 @@ class SecretsController @Inject() (
      * @param delete - True to delete, else just retrieve information.
      */
     def <~(organization: String, name: String, bot: String, username: String, target: String, delete: Boolean = false)
-          (implicit r: Request[AnyContent]): Future[Result] = {
-        if(r.headers.hasHeader(vaultHeader)) {
+          (implicit request: Request[AnyContent]): Future[Result] = {
+        validateThen {
             ~> {
-                val token = r.headers(vaultHeader)
-
+                val token = request.headers(vaultHeader)
                 HandleDataFor(
                     Metadata(organization, Some(name), Some(bot), Some(token)),
                     Content(username, None, None),
@@ -89,8 +89,6 @@ class SecretsController @Inject() (
                     delete=true
                 )
             }
-        } else {
-            Future.successful(BadRequest(s"Missing $vaultHeader header."))
         }
     }
 
@@ -104,6 +102,15 @@ class SecretsController @Inject() (
             case 503 => ServiceUnavailable(res._2)
             case 500 => InternalServerError(res._2)
             case any => InternalServerError(s"Couldn't handle request. $any")
+        }
+    }
+
+    def validateThen(content: Future[Result], requireVaultToken: Boolean = true)
+                    (implicit request: Request[AnyRef]): Future[Result] = {
+        if(request.validate(requireVaultToken = requireVaultToken)) {
+            content
+        } else {
+            Future.successful(BadRequest(s"Missing required headers."))
         }
     }
 }
